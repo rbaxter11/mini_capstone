77b44abd49e1eb02f477f21273459ac1e2e3d074
ryan <ryanandrewbaxter@gmail.com>
2020-10-28 13:57:16 -0500

    Adds individual routes for all 3 items

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -6,4 +6,23 @@ class Api::ProductsController < ApplicationController
     render 'products.json.jb'
     
   end
+
+  def display1
+    @prod1 = Product.first
+
+    render 'products.json.jb'
+  end
+
+  def display2
+    @prod2 = Product.second
+
+    render 'products.json.jb'
+  end
+
+  def display3
+    @prod3 = Product.third
+
+    render 'products.json.jb'
+  end
 end
+          
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/display_all_path" => "products#display_all"
+    get "/display_prod1" => "products#display1"
+    get "/display_prod2" => "products#display2"
+    get "/display_prod3" => "products#display3"
   end
 end
