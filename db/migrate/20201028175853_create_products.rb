7b1c4c8668cb71eb40c55b66c735807a8291eb5f
ryan <ryanandrewbaxter@gmail.com>
2020-10-28 13:00:53 -0500

    Generates new model: Product

---
+++ db/migrate/20201028175853_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :description
+      t.string :color
+
+      t.timestamps
+    end
+  end
+end
