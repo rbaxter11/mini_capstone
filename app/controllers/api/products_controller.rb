88c9c6621c3587771c0e8877456b5bf442af9fad
ryan <ryanandrewbaxter@gmail.com>
2020-10-30 14:16:05 -0500

    Adds query, url segment and body params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -24,5 +24,34 @@ class Api::ProductsController < ApplicationController
 
     render 'prod3.json.jb'
   end
+
+  def display_query_action
+    input_value = params["product_id"]
+    @output = Product.find_by(id: input_value)
+    render 'query.json.jb'
+  end
+  
+  def display_url_action
+    input_value = params["variable"]
+    @output = Product.find_by(id: input_value)
+    render 'query.json.jb'
+  end
+
+  def display_body_action
+    input_value = params["product_id"]
+    @output = Product.find_by(id: input_value)
+    render 'query.json.jb'
+  end
+
+  def login
+    input_name = params["name"]
+    input_password = params["password"]
+    if input_name == "hugh" && input_password == "swordfish"
+      @output = "Valid credentials"
+    else
+      @output = "Invalid credentials"
+    end
+    render 'login.json.jb'
+  end
 end
           
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     get "/display_prod1" => "products#display1"
     get "/display_prod2" => "products#display2"
     get "/display_prod3" => "products#display3"
-  end
+    get "/display_query_path" => "products#display_query_action"
+    get "/display_url_path/:variable" => "products#display_url_action"
+    post "/display_body_path" => "products#display_body_action"
+    post "/username_path" => "products#login"
+    end
 end
